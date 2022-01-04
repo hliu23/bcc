5543e904f092c08a2d86c7ca53c2a93d9975e36b
Hechen Liu <codingchallenge4@gmail.com>
2022-01-04 10:29:57 -0500

    add hero image to articles

--- src/pages/articles/{mdx.slug}.js
+++ src/pages/articles/{mdx.slug}.js
@@ -1,13 +1,29 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
 import Layout from "../../components/layout";
 
 const blogPost = ({ data }) => {
   console.log(data);
+
+  const image = getImage(data.mdx.frontmatter.hero_image);
+
   return (
   <Layout pageTitle={data.mdx.frontmatter.title}>
     <p>{data.mdx.frontmatter.date}</p>
+    <GatsbyImage 
+      image={image}
+      alt={data.mdx.frontmatter.hero_image_alt}
+    />
+    <p>
+      Photo Credit:{" "}
+      <a href={data.mdx.frontmatter.hero_image_link}>
+        {data.mdx.frontmatter.hero_image_credit}
+      </a>
+    </p>
     <MDXRenderer>
       {data.mdx.body}
     </MDXRenderer>
@@ -21,10 +37,19 @@ query ($id: String) {
     frontmatter {
 			title
       postDate(formatString: "MMMM D, YYYY")
+      hero_image {
+				childImageSharp {
+          gatsbyImageData
+        }
+      }
+      hero_image_alt
+      hero_image_link
+      hero_image_credit      
     }
     body
   }
-}`;
+}
+`;
 
 export default blogPost;
 
