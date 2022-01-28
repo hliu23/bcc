32a206f6b0af5a18081f534f30b01cb03e82329e
Hechen Liu <codingchallenge4@gmail.com>
2022-01-27 23:23:19 -0500

    setup project page

--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,39 @@
 import * as React from "react";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-import JSONData from "../content/projects/brebeuf-schedule-5754929/project.json";
 
 
 const Project = (props) => {
+  // imageData, name, authors, description, updatedDate, link
   return (
-    <p>
-      <h1>Hello World! {props.name}</h1>
-    <p>{props.children}</p>
-    <p>{JSONData.name}</p>
+    <div className="col-md-6">
+      <div className="card">
+        <p className="card-header text-center">
+          {props.status}
+        </p>
 
-    </p>
-    
+
+        <div className="card-body">
+          <h5 className="card-title text-center">{props.name}</h5>
+          <h6 className="card-subtitle text-center">{props.authors.map((author, index) => {
+            var ret = author;
+            if (index < props.authors.length - 1) ret += ", ";
+            return ret;
+          })}
+          </h6>
+          <GatsbyImage
+            image={props.imageData}
+            alt="icon"
+            className="card-img-top"
+            objectFit="contain"
+          ></GatsbyImage>
+          <p className="card-text">{props.description}</p>
+        </div>
+        <p className="card-link text-center">
+          <a href={props.link} className="btn btn-primary">View Project</a>
+        </p>
+      </div>
+    </div>
   )
 };
 
